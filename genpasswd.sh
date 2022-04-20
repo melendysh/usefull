0f8fcdc3943ed528463231bcaa1e533539fe2c09
melendysh <104088178+melendysh@users.noreply.github.com>
2022-04-20 12:36:15 -0400

    Update genpasswd.sh

--- genpasswd.sh
+++ genpasswd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#Generates a temp password by shaun
- cat /dev/urandom| tr -dc 'a-zA-Z0-9' | fold -w 20| head -1
+#Generates a temp password!
+
+cat /dev/urandom| tr -dc 'a-zA-Z0-9' | fold -w 26| head -1
